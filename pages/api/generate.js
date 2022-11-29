3aa6e8a58e6f580635af9892beb69002c7acb76b
alexdphan <alexphan0515@gmail.com>
2022-11-28 20:09:04 -0500

    upgraded to model text-davinci-003

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -17,7 +17,7 @@ const generateAction = async (req, res) => {
   console.log(`API: ${basePromptPrefix}${req.body.userInput}`);
 
   const baseCompletion = await openai.createCompletion({
-    model: 'text-davinci-002',
+    model: 'text-davinci-003',
     prompt: `${basePromptPrefix}${req.body.userInput}`,
     temperature: 0.8,
     max_tokens: 250,
